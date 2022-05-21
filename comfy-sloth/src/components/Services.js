3ee405d3ccf2d5790cfeaed3073edc5837be8319
josh2kv <josh2kv@gmail.com>
2022-05-21 17:18:59 +0900

    [comfy-sloth] add home page>services

--- comfy-sloth/src/components/Services.js
+++ comfy-sloth/src/components/Services.js
@@ -1,10 +1,39 @@
-import React from 'react'
-import styled from 'styled-components'
-import { services } from '../utils/constants'
+import React from 'react';
+import styled from 'styled-components';
+import { services } from '../utils/constants';
 
 const Services = () => {
-  return <h4>services </h4>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <article className="header">
+          <h3>
+            custom furniture <br />
+            built only for you
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ut
+            tenetur odio repellendus amet rem eligendi illo corrupti. Amet
+            beatae saepe odio illum, sequi eaque! Porro, sapiente minima! Est,
+            sunt.
+          </p>
+        </article>
+        <div className="services-center">
+          {services.map(service => {
+            const { id, icon, title, text } = service;
+            return (
+              <article className="service" key={id}>
+                <span className="icon">{icon}</span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   h3,
@@ -68,5 +97,5 @@ const Wrapper = styled.section`
       transform: translateY(5rem);
     }
   }
-`
-export default Services
+`;
+export default Services;
