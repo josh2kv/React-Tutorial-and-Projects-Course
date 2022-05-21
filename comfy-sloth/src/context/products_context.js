78055b17845f98c8b408c67e63e0cde9f9472df5
josh2kv <josh2kv@gmail.com>
2022-05-21 23:29:00 +0900

    [compfy-sloth]  add fetchSingleProduct

--- comfy-sloth/src/context/products_context.js
+++ comfy-sloth/src/context/products_context.js
@@ -19,6 +19,9 @@ const initialState = {
   products_error: false,
   products: [],
   featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
 };
 
 const ProductsContext = React.createContext();
@@ -39,10 +42,23 @@ export const ProductsProvider = ({ children }) => {
 
     try {
       const response = await axios.get(url);
-      const products = response.data;
-      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: response.data });
     } catch (error) {
       dispatch({ type: GET_PRODUCTS_ERROR });
+    }
+  };
+
+  const fetchSingleProduct = async url => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
+
+    try {
+      const response = await axios.get(url);
+      dispatch({
+        type: GET_SINGLE_PRODUCT_SUCCESS,
+        payload: response.data,
+      });
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
     }
   };
 
--- comfy-sloth/src/reducers/products_reducer.js
+++ comfy-sloth/src/reducers/products_reducer.js
@@ -19,7 +19,7 @@ const products_reducer = (state, action) => {
   }
 
   if (action.type === GET_PRODUCTS_BEGIN) {
-    return { ...state, product_loading: true };
+    return { ...state, products_loading: true, products_error: false };
   }
 
   if (action.type === GET_PRODUCTS_SUCCESS) {
@@ -29,14 +29,38 @@ const products_reducer = (state, action) => {
 
     return {
       ...state,
-      product_loading: false,
+      products_loading: false,
       products: action.payload,
       featured_products,
     };
   }
 
   if (action.type === GET_PRODUCTS_ERROR) {
-    return { ...state, product_loading: false, products_error: true };
+    return { ...state, products_loading: false, products_error: true };
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {
+      ...state,
+      single_product_loading: true,
+      single_product_error: false,
+    };
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product: action.payload,
+    };
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_ERROR) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: true,
+    };
   }
   return state;
   throw new Error(`No Matching "${action.type}" - action type`);
