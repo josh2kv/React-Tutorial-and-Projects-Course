d3c71caa24b5fdc9a2f902faf39a971690de9c93
josh2kv <josh2kv@gmail.com>
2022-06-01 17:57:21 +0900

    [jobster] add register request with redux-toolkit

--- jobster/src/pages/Register.js
+++ jobster/src/pages/Register.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
+import { toast } from 'react-toastify'
+import { useDispatch, useSelector } from 'react-redux'
+import { loginUser, registerUser } from '../features/user/userSlice'
 import { Logo, FormRow } from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage'
-import { toast } from 'react-toastify'
 
 const initialState = {
   name: '',
@@ -12,6 +14,8 @@ const initialState = {
 
 const Register = () => {
   const [values, setValues] = useState(initialState)
+  const { user, isLoading } = useSelector(store => store.user)
+  const dispatch = useDispatch()
 
   const handleChange = e => {
     const name = e.target.name
@@ -24,7 +28,13 @@ const Register = () => {
     const { name, email, password, isMember } = values
     if (!email || !password || (!isMember && !name)) {
       toast.error('Please fill out all fields')
+      return
     }
+    if (isMember) {
+      dispatch(loginUser({ email: email, password: password }))
+      return
+    }
+    dispatch(registerUser({ name, email, password }))
   }
 
   const toggleMember = () => {
@@ -55,7 +65,7 @@ const Register = () => {
           value={values.password}
           handleChange={handleChange}
         />
-        <button type="submit" className="btn btn-block">
+        <button type="submit" className="btn btn-block" disabled={isLoading}>
           submit
         </button>
         <p>
