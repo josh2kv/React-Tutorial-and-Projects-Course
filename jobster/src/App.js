e131ed30661401f7a8e388fef2fcb1e9ee51061a
josh2kv <josh2kv@gmail.com>
2022-06-01 13:27:52 +0900

    [jobster] set react-router

--- jobster/src/App.js
+++ jobster/src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
-import Landing from "./pages/Landing";
-import styled from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing, Error, Dashboard, Register } from "./pages";
 
 const App = () => {
     return (
-        <div>
-            <Landing />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="landing" element={<Landing />} />
+                <Route path="register" element={<Register />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
--- jobster/src/pages/Landing.js
+++ jobster/src/pages/Landing.js
@@ -1,4 +1,4 @@
-import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
@@ -17,7 +17,9 @@ const Landing = () => {
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque a numquam facere beatae
                         architecto illo praesentium blanditiis repellat facilis aliquam!
                     </p>
-                    <button className="btn btn-hero">Login/Register</button>
+                    <Link to="/register" className="btn btn-hero">
+                        Login/Register
+                    </Link>
                 </div>
                 <img src={main} alt="job hunt" className="img main-img" />
             </div>
