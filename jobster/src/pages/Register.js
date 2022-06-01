aa13d6be63b4743fea17eecc8ddddabcd25ac44b
josh2kv <josh2kv@gmail.com>
2022-06-01 14:02:58 +0900

    [jobster] add register page

--- jobster/src/pages/Register.js
+++ jobster/src/pages/Register.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import { Logo, FormRow } from '../components'
+import Wrapper from '../assets/wrappers/RegisterPage'
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+}
 
 const Register = () => {
-    return <div>Register</div>;
-};
+  const [values, setValues] = useState(initialState)
 
-export default Register;
+  const handleChange = e => {
+    console.log(e.target)
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log(e.target)
+  }
+  return (
+    <Wrapper>
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Register</h3>
+        <FormRow
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+        />
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  )
+}
+
+export default Register
