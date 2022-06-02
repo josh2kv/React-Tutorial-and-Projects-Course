e36f38a0e4f702669494777d9aaac5c9b30d0b22
josh2kv <josh2kv@gmail.com>
2022-06-02 21:50:07 +0900

    [jobster] add NavLinks (layout is all set)

--- jobster/src/components/BigSidebar.js
+++ jobster/src/components/BigSidebar.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import Logo from './Logo'
 import Wrapper from '../assets/wrappers/BigSidebar'
+import NavLinks from './NavLinks'
 
 const BigSidebar = () => {
-  return <Wrapper>BigSidebar</Wrapper>
+  const { isSidebarOpen } = useSelector(store => store.user)
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen ? 'sidebar-container' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 export default BigSidebar
--- jobster/src/components/SmallSidebar.js
+++ jobster/src/components/SmallSidebar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
-import { NavLink } from 'react-router-dom'
 import Logo from './Logo'
 import { useSelector, useDispatch } from 'react-redux'
 import { toggleSidebar } from '../features/user/userSlice'
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import NavLinks from './NavLinks'
 
 const SmallSidebar = () => {
   const { isSidebarOpen } = useSelector(store => store.user)
@@ -27,7 +27,7 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">nav links</div>
+          <NavLinks toggleSidebar={toggle} />
         </div>
       </div>
     </Wrapper>
