e749ed77239a07529d91e2c3d193a465baf2f6fe
josh2kv <josh2kv@gmail.com>
2022-06-03 22:34:42 +0900

    [jobster] set job slice

--- jobster/src/store.js
+++ jobster/src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './features/user/userSlice';
 import allJobsSlice from './features/allJobs/allJobsSlice';
+import jobSlice from './features/job/jobSlice';
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
     allJobs: allJobsSlice,
+    job: jobSlice,
   },
 });
