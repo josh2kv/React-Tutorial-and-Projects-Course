09ee3beab01b9b1ec28e2d7a743e957c7c09d30e
josh2kv <josh2kv@gmail.com>
2022-06-04 01:10:10 +0900

    [jobster] complete addJob

--- jobster/src/features/job/jobSlice.js
+++ jobster/src/features/job/jobSlice.js
@@ -3,6 +3,29 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 import customFetch from '../../utils/axios';
 import { getUserFromLocalStorage } from '../../utils/localStorage';
+// Slice 안에서는 useSelector를 쓰지 않고 reducers를 직접 import해서 씀
+import { logoutUser } from '../user/userSlice';
+
+export const createJob = createAsyncThunk(
+  'job/createJob',
+  async (job, thunkAPI) => {
+    try {
+      const res = await customFetch.post('/jobs', job, {
+        headers: {
+          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+        },
+      });
+      thunkAPI.dispatch(clearValues());
+      return res.data;
+    } catch (error) {
+      if (error.response.status === 401) {
+        thunkAPI.dispatch(logoutUser());
+        return thunkAPI.rejectWithValue('Unauthorized: Logging Out...');
+      }
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  },
+);
 
 const initialState = {
   isLoading: false,
@@ -20,6 +43,31 @@ const initialState = {
 const jobSlice = createSlice({
   name: 'job',
   initialState,
+  reducers: {
+    handleChange: (state, { payload: { name, value } }) => {
+      state[name] = value;
+    },
+    clearValues: () => {
+      return {
+        ...initialState,
+        jobLocation: getUserFromLocalStorage()?.location || '',
+      };
+    },
+  },
+  extraReducers: {
+    [createJob.pending]: state => {
+      state.isLoading = true;
+    },
+    [createJob.fulfilled]: state => {
+      state.isLoading = false;
+      toast.success('Job Created');
+    },
+    [createJob.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+  },
 });
 
+export const { handleChange, clearValues } = jobSlice.actions;
 export default jobSlice.reducer;
--- jobster/src/pages/dashboard/AddJob.js
+++ jobster/src/pages/dashboard/AddJob.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 import { FormRow, FormRowSelect } from '../../components';
 import Wrapper from '../../assets/wrappers/DashboardFormPage';
+import {
+  handleChange,
+  clearValues,
+  createJob,
+} from '../../features/job/jobSlice';
 
 const AddJob = () => {
   const {
@@ -17,6 +22,8 @@ const AddJob = () => {
     isEditing,
     editJobId,
   } = useSelector(store => store.job);
+  const { user } = useSelector(store => store.user);
+  const dispatch = useDispatch();
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -25,12 +32,25 @@ const AddJob = () => {
       toast.error('Please fill out all fields');
       return;
     }
+
+    dispatch(createJob({ position, company, jobLocation, jobType, status }));
   };
 
   const handleJobInput = e => {
     const name = e.target.name;
     const value = e.target.value;
+
+    dispatch(handleChange({ name, value }));
   };
+
+  useEffect(() => {
+    dispatch(
+      handleChange({
+        name: 'jobLocation',
+        value: user.location,
+      }),
+    );
+  }, []);
 
   return (
     <Wrapper>
@@ -73,7 +93,7 @@ const AddJob = () => {
             <button
               type="button"
               className="btn btn-block clear-btn"
-              onClick={() => console.log('clear values')}
+              onClick={() => dispatch(clearValues())}
             >
               clear
             </button>
